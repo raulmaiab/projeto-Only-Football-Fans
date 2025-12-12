c7bc551a692d605de3b3cf3f87e57b6adc2b5627
Vitor Gadelha <vrlbga@cesar.school>
2025-12-11 21:16:24 -0300

    atualizando o forms criado

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,26 @@
-from django import forms
-from .models import UserProfile
-
 class EditarPerfilForm(forms.ModelForm):
+    email = forms.EmailField()
+    nome = forms.CharField(max_length=150)
+
     class Meta:
         model = UserProfile
-        fields = [
-            'nome_completo',
-            'bio',
-            'instagram',
-            'twitter',
-            'site',
-            'foto_perfil',
-        ]
+        fields = ['time']  # só o campo do profile
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+        self.fields['email'].initial = user.email
+        self.fields['nome'].initial = user.first_name
+        self.user = user
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+
+        # salva no User
+        self.user.email = self.cleaned_data['email']
+        self.user.first_name = self.cleaned_data['nome']
+        if commit:
+            self.user.save()
+            profile.save()
+        return profile
