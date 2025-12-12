b7a4ead1a2ee7f083b246e856247df1afe187736
Vitor Gadelha <vrlbga@cesar.school>
2025-12-11 21:29:06 -0300

    corrigindo bug do forms

--- core/forms.py
+++ core/forms.py
@@ -1,26 +1,34 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+Usuario = get_user_model()
+
 class EditarPerfilForm(forms.ModelForm):
-    email = forms.EmailField()
-    nome = forms.CharField(max_length=150)
+    nome = forms.CharField(max_length=150, label='Nome')
+    email = forms.EmailField(label='E-mail')
 
     class Meta:
-        model = UserProfile
-        fields = ['time']  # só o campo do profile
+        model = Usuario
+        fields = ['time_favorito']  # Campo do modelo
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
         super().__init__(*args, **kwargs)
 
+        # Inicializa os campos extras
         self.fields['email'].initial = user.email
         self.fields['nome'].initial = user.first_name
         self.user = user
 
     def save(self, commit=True):
-        profile = super().save(commit=False)
-
-        # salva no User
-        self.user.email = self.cleaned_data['email']
+        # Atualiza os campos do usuário
         self.user.first_name = self.cleaned_data['nome']
+        self.user.email = self.cleaned_data['email']
         if commit:
             self.user.save()
+        
+        # Salva campos do perfil (se houver)
+        profile = super().save(commit=False)
+        if commit:
             profile.save()
         return profile
